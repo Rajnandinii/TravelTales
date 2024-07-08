b33e1ce8daac3c5595a18f19aa6add4fe656bd11
Rajnandinii <imnandinipatel@gmail.com>
2024-07-08 23:34:41 +0530

    updated client

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,80 @@
+import { Footer } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsLinkedin } from 'react-icons/bs';
+export default function FooterCom() {
+  return (
+    <Footer container className='border border-t-8 border-teal-500'>
+      <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+          <div className='mt-5'>
+            <Link
+              to='/'
+              className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
+            >
+              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+                Devvrat's
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+            <div>
+              <Footer.Title title='About' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://newbreak.church/blog/?gad_source=1'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Relevant Blog Website
+                </Footer.Link>
+                <Footer.Link
+                  href='/about'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Devvrat's Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Follow us' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://www.github.com/devvrat011'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href='#'>Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Legal' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='#'>Privacy Policy</Footer.Link>
+                <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className='w-full sm:flex sm:items-center sm:justify-between'>
+          <Footer.Copyright
+            href='#'
+            by="Devvrat's blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href='https://www.facebook.com/devvrat.gupta.39' icon={BsFacebook}/>
+            <Footer.Icon href='https://www.instagram.com/devvrat_gupta11/' icon={BsInstagram}/>
+            <Footer.Icon href='https://twitter.com/Devvrat6353' icon={BsTwitter}/>
+            <Footer.Icon href='https://github.com/devvrat011' icon={BsGithub}/>
+            <Footer.Icon href='https://www.linkedin.com/in/devvrat-gupta-84a979228/' icon={BsLinkedin}/>
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
